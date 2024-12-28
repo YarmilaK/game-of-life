5e9290dabda08faf2492d3a5a161ec119d7055ec
Yarmila <klishinael@gmail.com>
2024-12-28 16:04:01 +0400

    add counter

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def index():
 def live():
     game = GameOfLife()
     game.form_new_generation()
+    game.gen_cnt += 1
     return render_template('live.html', game = game)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
